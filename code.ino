30c54c59705347e600d03278819a74c48c4b755a
Phillip Roos <55896656+philliproos01@users.noreply.github.com>
2023-01-24 13:13:02 -0500

    update 2

--- code.ino
+++ code.ino
@@ -31,9 +31,10 @@ void loop() {
       digitalWrite(11, LOW);
       delay(1000);   
     }
-  
-  digitalWrite(13, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(13, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);
+  }
+  //THIS WORKS
+  //digitalWrite(13, HIGH);  // turn the LED on (HIGH is the voltage level)
+  //delay(1000);                      // wait for a second
+  //digitalWrite(13, LOW);   // turn the LED off by making the voltage LOW
+  //delay(1000);
 }
