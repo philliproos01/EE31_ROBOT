3962619876a520c05f709421ae21030616ec84df
Phillip Roos <55896656+philliproos01@users.noreply.github.com>
2023-04-23 03:22:26 -0400

    Create phase6.ino

---
+++ phase6.ino
@@ -0,0 +1,32 @@
+int LED = 7;
+int LED2 = 6;
+int val = 0;
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(9, INPUT_PULLUP);
+  digitalWrite(LED, HIGH);
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);      
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  
+    
+  val = digitalRead(9);
+  Serial.println(val);// wait for a second
+  //if (val =)
+  if (digitalRead(9) == HIGH) {
+    digitalWrite(LED2, HIGH);
+    //WRITE CODE TO USE WIFI TO MAKE OTHER ARDUINO MOVE FORWARD FOR 5 SECONDS
+
+
+    
+  } else if (digitalRead(9) == LOW) {
+    digitalWrite(LED2, LOW);
+  }
+}
